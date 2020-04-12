97d861302ebc692cf817e715a5a3c13a66d99588
Pedro Pessoa <pessoa.pedro91@gmail.com>
2020-04-12 15:45:57 +0300

    Add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+};
