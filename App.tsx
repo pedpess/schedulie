87e31f46d39ae5719aeb334e4ca75c61febb73e3
Pedro Pessoa <pessoa.pedro91@gmail.com>
2020-04-12 14:41:16 +0300

    Add initial screen for opening hours

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import styled from "styled-components/native";
+import { OpeningHours } from "./src/screens";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Container>
+      <OpeningHours />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
