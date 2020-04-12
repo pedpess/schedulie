5793ede85a6f375f4f691c345cd91ff0fb9a73ed
Pedro Pessoa <pessoa.pedro91@gmail.com>
2020-04-12 15:36:22 +0300

    Add custom hook to load expo custom fonts

---
+++ src/hooks/useCustomFont.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import * as Font from "expo-font";
+
+export default () => {
+  const [fontLoaded, setFontLoaded] = useState<boolean>(false);
+
+  function fetchCustomFonts() {
+    return Font.loadAsync({
+      "roboto-bold": require("../../assets/fonts/Roboto-Bold.ttf"),
+      "roboto-medium": require("../../assets/fonts/Roboto-Medium.ttf"),
+      "roboto-regular": require("../../assets/fonts/Roboto-Regular.ttf"),
+    });
+  }
+
+  useEffect(() => {
+    if (!fontLoaded) {
+      setFontLoaded(true);
+      fetchCustomFonts();
+    }
+  }, [fontLoaded]);
+
+  return fontLoaded;
+}
