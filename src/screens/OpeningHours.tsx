e6c9aae9bb0dbea36a86489a8dc0fef3c14c30e2
Pedro Pessoa <pessoa.pedro91@gmail.com>
2020-04-21 01:05:06 +0300

    Remove unnecessary headerIcon props

--- src/screens/OpeningHours.tsx
+++ src/screens/OpeningHours.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Card } from '../components';
 
 export const OpeningHours: React.FC = () => {
-  return <Card headerTitle="Opening Hours" headerIcon />;
+  return <Card headerTitle="Opening Hours" />;
 };
